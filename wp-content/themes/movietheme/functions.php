4fcf311dc0c0538e42b4fa0500103eda7cde583a
Daniel Daniel <info@daan-webdesign.nl>
2021-11-19 11:25:46 +0100

    Commit 3 WP theme with Bootstrap 5

---
+++ wp-content/themes/movietheme/functions.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * Movie theme Functions
+ *
+ * @package Movietheme
+ * @since Movietheme 1.0
+ */
+
+add_theme_support( 'post-thumbnails' );
---
+++ wp-content/themes/movietheme/index.php
@@ -0,0 +1,36 @@
+<?php get_header(); ?>
+
+<div class = "container">
+
+ 	<div class="jumbotron">
+	  <h1 class="display-4">Hello, world!</h1>
+	  <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+	  <hr class="my-4">
+	  <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+	  <p class="lead">
+	    <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+	  </p>
+ 	</div>
+
+	<div class = "panel panel-default panel-body">
+		<div class = "row">
+			<div class = "col-md-2">
+				<ul class="nav nav-pills nav-stacked">
+					<li><a href = "#">Category One</a></li>
+					<li><a href = "#">Category Two</a></li>
+					<li><a href = "#">Category Three</a></li>
+					<li><a href = "#">Category Four</a></li>
+				</ul>
+			</div>
+			<div class = "col-md-10">
+				<a href = "#"><h3>This random post is really awesome!</h3></a>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc arcu erat, congue eget ornare et, luctus eget purus. Etiam et risus metus. Nam sed mi tellus. Mauris molestie massa eu metus tempor, in hendrerit arcu adipiscing. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+				<p class = "text-muted">Posted by Daniel on January 1st 2021</p>
+			</div>
+		</div>
+	</div>
+
+		</div>
+
+
+<?php get_footer(); ?>
